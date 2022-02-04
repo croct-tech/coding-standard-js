99f3d6a26dd1094e759fc6028be448a5bab3bff7
Marcos Passos <marcos@croct.com>
2022-02-04 18:41:55 -0300

    WIP

---
+++ src/argument-indentation/index.test.ts
@@ -0,0 +1,190 @@
+import {ESLintUtils} from '@typescript-eslint/utils';
+import rule from './index';
+
+const ruleTester = new ESLintUtils.RuleTester({
+    parser: '@typescript-eslint/parser',
+});
+
+ruleTester.run('argument-spacing', rule, {
+    valid: [
+        {
+            code: 'new Example(1, 2, 3);',
+        },
+        {
+            code: `new Example(
+                1,
+                2,
+                3,
+            );`,
+        },
+        {
+            code: 'useEffect(() => console.log(1), []);',
+        },
+        {
+            code: `useEffect(
+                () => console.log(1),
+                [],
+            );`,
+        },
+    ],
+    invalid: [
+        {
+            code: `new Example(1,
+            2,
+            3);`,
+            output: `new Example(
+1,
+            2,
+            3
+);`,
+            errors: [
+                {
+                    messageId: 'missing',
+                    line: 1,
+                    endLine: 1,
+                    column: 13,
+                    endColumn: 13,
+                },
+                {
+                    messageId: 'missing',
+                    line: 3,
+                    endLine: 3,
+                    column: 14,
+                    endColumn: 14,
+                },
+            ],
+        },
+        {
+            code: `new Example(1,
+            2,
+            3
+            );`,
+            output: `new Example(
+1,
+            2,
+            3
+            );`,
+            errors: [
+                {
+                    messageId: 'missing',
+                    line: 1,
+                    endLine: 1,
+                    column: 13,
+                    endColumn: 13,
+                },
+            ],
+        },
+        {
+            code: `useEffect(() => {
+                console.log(1);
+            }, []);`,
+            output: `useEffect(
+() => {
+                console.log(1);
+            },
+[]
+);`,
+            errors: [
+                {
+                    messageId: 'missing',
+                    line: 1,
+                    endLine: 1,
+                    column: 11,
+                    endColumn: 11,
+                },
+                {
+                    messageId: 'missing',
+                    line: 3,
+                    endLine: 3,
+                    column: 15,
+                    endColumn: 16,
+                },
+                {
+                    messageId: 'missing',
+                    line: 3,
+                    endLine: 3,
+                    column: 18,
+                    endColumn: 18,
+                },
+            ],
+        },
+        {
+            code: `useEffect(() => {
+                console.log(1);
+            },
+            []);`,
+            output: `useEffect(
+() => {
+                console.log(1);
+            },
+            []
+);`,
+            errors: [
+                {
+                    messageId: 'missing',
+                    line: 1,
+                    endLine: 1,
+                    column: 11,
+                    endColumn: 11,
+                },
+                {
+                    messageId: 'missing',
+                    line: 4,
+                    endLine: 4,
+                    column: 15,
+                    endColumn: 15,
+                },
+            ],
+        },
+        {
+            code: `useEffect(() => {
+                console.log(1);
+            },
+            []
+            );`,
+            output: `useEffect(
+() => {
+                console.log(1);
+            },
+            []
+            );`,
+            errors: [
+                {
+                    messageId: 'missing',
+                    line: 1,
+                    endLine: 1,
+                    column: 11,
+                    endColumn: 11,
+                },
+            ],
+        },
+        {
+            code: `useEffect(
+            () => {
+                console.log(1);
+            }, []);`,
+            output: `useEffect(
+            () => {
+                console.log(1);
+            },
+[]
+);`,
+            errors: [
+                {
+                    messageId: 'missing',
+                    line: 4,
+                    endLine: 4,
+                    column: 15,
+                    endColumn: 16,
+                },
+                {
+                    messageId: 'missing',
+                    line: 4,
+                    endLine: 4,
+                    column: 18,
+                    endColumn: 18,
+                },
+            ],
+        },
+    ],
+});
---
+++ src/argument-indentation/index.ts
@@ -0,0 +1,78 @@
+import {TSESTree} from '@typescript-eslint/types';
+import {createRule} from '../createRule';
+
+const rule = createRule({
+    name: 'argument-spacing',
+    meta: {
+        type: 'suggestion',
+        docs: {
+            description: 'Enforces consistent spacing in function call arguments.',
+            recommended: 'error',
+        },
+        fixable: 'whitespace',
+        schema: [],
+        messages: {
+            missing: 'Missing new line.',
+        },
+    },
+    defaultOptions: [
+        {
+            before: false,
+            after: true,
+        },
+    ],
+    create(context) {
+        function check(node: TSESTree.NewExpression | TSESTree.CallExpression) {
+            const sourceCode = context.getSourceCode();
+            const firstToken = sourceCode.getFirstToken(node)!;
+            const lastToken = sourceCode.getLastToken(node)!;
+
+            if (node.arguments.length === 0 || firstToken.loc.start.line === lastToken.loc.end.line) {
+                return;
+            }
+
+            const tokens = [
+                sourceCode.getTokenBefore(node.arguments[0], {
+                    filter: token => token.value === '(',
+                }),
+                ...node.arguments,
+                sourceCode.getTokenAfter(node.arguments[node.arguments.length - 1], {
+                    filter: token => token.value === ')',
+                }),
+            ];
+
+            for (let i = 1; i < tokens.length; i++) {
+                const previousToken = tokens[i - 1]!;
+                const currentToken = tokens[i]!;
+
+                if (previousToken.loc.end.line === currentToken.loc.start.line) {
+                    const tokenBefore = sourceCode.getTokenBefore(
+                        currentToken,
+                        {includeComments: true},
+                    )!;
+
+                    context.report({
+                        node: node,
+                        loc: {
+                            start: tokenBefore.loc.end,
+                            end: currentToken.loc.start,
+                        },
+                        messageId: 'missing',
+                        fix: fixer => fixer.replaceTextRange([tokenBefore.range[1], currentToken.range[0]], '\n'),
+                    });
+                }
+            }
+        }
+
+        return {
+            CallExpression(node) {
+                check(node);
+            },
+            NewExpression(node) {
+                check(node);
+            },
+        };
+    },
+});
+
+export default rule;
