4403046cfd79d6f58db961ba2cfb8879ae2e48d4
Marcos Passos <marcos@croct.com>
2022-02-08 15:43:59 -0300

    WIP

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,7 +1,9 @@
 import {argumentSpacing} from './argument-spacing';
 import {jsxAttributeSpacing} from './jsx-attribute-spacing';
+import {complexExpressionSpacing} from './complex-expression-spacing';
 
 export const rules = {
     'argument-spacing': argumentSpacing,
     'jsx-attribute-spacing': jsxAttributeSpacing,
+    'complex-expression-spacing': complexExpressionSpacing,
 };
