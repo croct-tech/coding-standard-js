f6f52ef6bd501a2958c568c2583723062fb51504
georgekaran <georgekaran12@gmail.com>
2022-03-11 19:27:07 -0300

    Add sort-keys auto fixable rule

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -14,6 +14,7 @@ export const react = {
         'testing-library',
         'jest-dom',
         '@croct',
+        'sort-keys-fix',
     ],
     rules: {
         '@croct/jsx-attribute-spacing': 'error',
@@ -122,7 +123,7 @@ export const react = {
         {
             files: ['**/styles.ts'],
             rules: {
-                'sort-keys': 'error',
+                'sort-keys-fix/sort-keys-fix': 'error',
             },
         },
     ],
