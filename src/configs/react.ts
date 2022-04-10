9e93d885cabc167e30e008ab53b722e9fd9f2f46
George Karan <georgekaran12@gmail.com>
2022-04-10 18:21:26 -0300

    Add jsx-sort-props rule

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -19,6 +19,12 @@ export const react = {
         '@croct/jsx-attribute-spacing': 'error',
         'react/jsx-wrap-multilines': 'error',
         'react/display-name': 'off',
+        'react/jsx-sort-props': [
+            'error',
+            {
+                multiline: 'last',
+            },
+        ],
         'jsx-quotes': [
             'error',
             'prefer-double',
